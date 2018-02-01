5e6441270531c6e21d0ea64ef815f9a457012156
Adam Saleh <asaleh@redhat.com>
2018-02-01 18:48:07 +0100

    Update

--- files/scripts/credentials.groovy
+++ files/scripts/credentials.groovy
@@ -3,16 +3,18 @@ import com.cloudbees.plugins.credentials.*
 import com.cloudbees.plugins.credentials.common.*
 import com.cloudbees.plugins.credentials.domains.*
 import com.cloudbees.plugins.credentials.impl.*
-import org.jenkinsci.plugins.plaincredentials.impl.*
-import hudson.util.Secret
 
 def credId = args[0]
 def description = args[1]
-def secret = args[2]
+def username = args[2]
+def password = args[3]
 
-def domain = Domain.global()
 def store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
-
-def secretText = new StringCredentialsImpl(CredentialsScope.GLOBAL,credId,description, Secret.fromString(secret))
-
-store.addCredentials(domain, secretText)
+def secret = new UsernamePasswordCredentialsImpl(
+      CredentialsScope.GLOBAL,
+      credId,
+      description,
+      username,
+      password
+      )
+store.addCredentials(Domain.global(), secret)
---
+++ files/scripts/credentials_example.sh
@@ -0,0 +1,1 @@
+java -jar jenkins-cli.jar -remoting -s $url -user $user groovy credentials.groovy "example_secret" "What are the magic words?" "adamsaleh" "squeamishossifrage" 
